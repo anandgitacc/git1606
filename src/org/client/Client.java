5cdf63db21d04dffc240ffde58fc427446f36483
anandgitacc <anand.akmail@gmail.com>
2023-06-16 08:56:19 +0530

    balagit

--- src/org/client/Client.java
+++ src/org/client/Client.java
@@ -9,5 +9,9 @@ public class Client {
 	public void clLocation() {
 		System.out.println("Client Location: Chennai");
 	}
+	
+	private void clID() {
+		System.out.println("Client id: 1122");
+	}
 
 }
